3b4d9257d17ca9125985a3b7de1c271834984935
Metastone <hugo_boulanger@hotmail.fr>
2021-01-30 20:27:58 +0100

    Make description parser handle lists

--- description_parser.py
+++ description_parser.py
@@ -5,26 +5,43 @@ def parse_description(file_path):
     for line in f.readlines():
         tokens += line.split()
 
-    description = []
+    description = [[]]
+    size_stack = []
+    name_stack = []
+    
     i = 0
     while i < len(tokens):
+        current_item_list = description[len(description) - 1]
+
         if is_identifier(tokens[i]):
             name = tokens[i]
-            print(name)
             i += 2
             if tokens[i] == "int":
-                description += [(name, "int")]
+                # Add item to current level
+                current_item_list += [(name, "int")]
             else:
-                size = tokens[i]
-                items = []
-                while tokens[i] != "]":
-                    i += 1
-                description += [(name, size, items)]
+                # This is the beginning of a list. Go down a level.
+                description += [[]]
+                size_stack += [tokens[i]]
+                name_stack += [name]
+                i += 1
+        elif tokens[i] == "]":
+            # End of a list. Go up a level.
+            size = size_stack[len(size_stack) - 1]
+            name = name_stack[len(name_stack) -1]
+            item_list = current_item_list
+            description.pop()
+            new_current_item_list = description[len(description) - 1]
+            new_current_item_list += [(name, size, item_list)]
+            size_stack.pop()
+            name_stack.pop() 
+
         i += 1
 
-    print(description)
+    return description[0]
 
 
 
 def is_identifier(x):
     return x.replace('_', '').isalnum()
+
